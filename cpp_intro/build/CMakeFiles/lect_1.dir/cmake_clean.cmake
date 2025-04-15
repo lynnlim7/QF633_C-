e8bbf7c1d6b74f8ed8b51f4cc157fdd9d18132ff
lynnlim99 <lynnlim99@hotmail.com>
2025-04-15 21:05:11 +0800

    lect_1_intro

---
+++ cpp_intro/build/CMakeFiles/lect_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lect_1.dir/lect_1.cpp.o"
+  "CMakeFiles/lect_1.dir/lect_1.cpp.o.d"
+  "lect_1"
+  "lect_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lect_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
